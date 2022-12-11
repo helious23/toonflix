0dd847b7037c63aadddd12fa65fa121bde07d751
Max16 <max16@naver.com>
2022-12-11 09:26:23 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 10,
+          title: const Text("Hello Flutter!"),
+        ),
+        body: const Center(
+          child: Text("Hello World!"),
+        ),
+      ),
+    );
+  }
+}
