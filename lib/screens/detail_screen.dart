99467cd6234706342b2c96b17a6441a8d2a9b409
Max16 <max16@naver.com>
2022-12-16 19:56:25 +0900

    Episodes

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -44,43 +44,37 @@ class _DetailScreenState extends State<DetailScreen> {
         elevation: 3,
       ),
       backgroundColor: Colors.white,
-      body: Hero(
-        tag: widget.id,
-        child: Column(children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 250,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        offset: const Offset(8, 8),
-                        color: Colors.black.withOpacity(0.4),
-                      )
-                    ]),
-                child: Image.network(widget.thumb),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          FutureBuilder(
-            future: webtoon,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 50,
-                  ),
-                  child: Column(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          offset: const Offset(8, 8),
+                          color: Colors.black.withOpacity(0.4),
+                        )
+                      ]),
+                  child: Image.network(widget.thumb),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            FutureBuilder(
+              future: webtoon,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
@@ -99,17 +93,64 @@ class _DetailScreenState extends State<DetailScreen> {
                         ),
                       ),
                     ],
+                  );
+                }
+                return const Center(
+                  child: CircularProgressIndicator(
+                    strokeWidth: 1,
                   ),
                 );
-              }
-              return const Center(
-                child: CircularProgressIndicator(
-                  strokeWidth: 1,
-                ),
-              );
-            },
-          )
-        ]),
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            FutureBuilder(
+              future: episodes,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: [
+                      for (var episode in snapshot.data!)
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(
+                                color: Colors.green.shade400,
+                                width: 1,
+                              )),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 20,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  episode.title,
+                                  style: TextStyle(
+                                    color: Colors.green.shade400,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.chevron_right_sharp,
+                                  color: Colors.green.shade400,
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                    ],
+                  );
+                }
+                return Container();
+              },
+            )
+          ]),
+        ),
       ),
     );
   }
--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -28,22 +28,19 @@ class Webtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          Hero(
-            tag: id,
-            child: Container(
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 15,
-                      offset: const Offset(8, 8),
-                      color: Colors.black.withOpacity(0.4),
-                    )
-                  ]),
-              child: Image.network(thumb),
-            ),
+          Container(
+            width: 250,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(8, 8),
+                    color: Colors.black.withOpacity(0.4),
+                  )
+                ]),
+            child: Image.network(thumb),
           ),
           const SizedBox(
             height: 12,
